bdf83090fd44c13d3c60237790bbd55ca037ffd4
Rodrigo Vitoriense <rodrigovitoriense@gmail.com>
2021-04-23 09:12:08 -0300

    Update script

--- base-apparel-coming-soon/script/main.js
+++ base-apparel-coming-soon/script/main.js
@@ -1,31 +1,51 @@
 const buttonSubmit = document.querySelector(".section-button")
-     
-    buttonSubmit.addEventListener("click", validateEmail)
-    
-    function validateEmail() {
+const form = document.querySelector('.section-content-form')
 
-      const email = document.getElementById("email").value
-      const pattern = /^[^ ]+@[^ ]+\.[a-z]{2,3}$/
-      const message = document.querySelector('.input-message')
-      const iconError = document.querySelector(".input-icon-error")
-      const inputEmail = document.getElementById("email")
+buttonSubmit.addEventListener("click", submitForm)
 
-      inputEmail.focus()
 
-      if (email.match(pattern)) {
+function validateEmail(email) {
 
-        alert("Registered Email!")
-        message.style.visibility = "hidden"
-        iconError.style.visibility = "hidden"
-        buttonSubmit.classList.remove("section-button-active")
+  const pattern = /^[^ ]+@[^ ]+\.[a-z]{2,3}$/
 
-      } else {
-        buttonSubmit.classList.add("section-button-active")
-        message.innerHTML = "Please provide a valide e-mail"
-        message.style.visibility = "visible"
-        iconError.style.visibility = "visible"
-        document.onload
+  return email.match(pattern)
+}
 
-      }
+function submitForm() {
 
+  form.addEventListener("submit", (e) => {
+
+    const inputEmail = document.getElementById("email")
+    const message = document.querySelector('.input-message')
+    const iconError = document.querySelector(".input-icon-error")
+
+    inputEmail.focus()
+
+    if (validateEmail(inputEmail.value)) {
+
+      alert("Registered Email!")
+      message.style.visibility = "hidden"
+      iconError.style.visibility = "hidden"
+      buttonSubmit.classList.remove("section-button-active")
+      
+
+    } else {
+      message.innerHTML = "Please provide a valide e-mail"
+      message.style.visibility = "visible"
+      iconError.style.visibility = "visible"
+      buttonSubmit.classList.add("section-button-active")
+      e.preventDefault()
     }
+
+    document.addEventListener("mousedown", (e) => {
+      
+      inputEmail.blur()
+      message.style.visibility = "hidden"
+      iconError.style.visibility = "hidden"
+      buttonSubmit.classList.remove("section-button-active")
+    })
+  })
+
+}
+
+
