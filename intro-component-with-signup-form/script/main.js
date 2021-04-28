ffe2c27f44117a26c5a5e67d5a2e132bab90ecc8
Rodrigo Vitoriense <rodrigovitoriense@gmail.com>
2021-04-27 22:30:59 -0300

    Update script into-component project

--- intro-component-with-signup-form/script/main.js
+++ intro-component-with-signup-form/script/main.js
@@ -1,48 +1,75 @@
 const form = document.querySelector(".main-form")
 
 
-form.addEventListener ("submit", (e) => {
-    debugger
-    const firstName = document.getElementById("firstName")
-    const LastName = document.getElementById("LastName")
-    const email = document.getElementById("email")
-    const password = document.getElementById("password")
-    const inputs = document.querySelectorAll(".main-form input")
-    let invalid = false
+form.addEventListener("submit", (e) => {
 
-  inputs.forEach(element => {
-    if(isEmpty(element)) {
-        element.focus()
-        element.textContent = "Digite alguma coisa"
-        invalid = true
-    }
-  })
+  const firstName = document.getElementById("firstName")
+  const LastName = document.getElementById("LastName")
+  const email = document.getElementById("email")
+  const password = document.getElementById("password")
 
-  if(invalid) {
-      e.preventDefault()
+
+  if (validateForm(firstName, LastName, email, password) === 0) {
+    alert("Formulário enviado")
+
   } else {
-      alert("formulario enviado")
+    e.preventDefault()
+    resetSubmit()
   }
-    
 })
 
+function validateForm(firstName, LastName, email, password) {
 
-function isEmpty (field) {
-   return field.value === ""
+  let hashValidate = 0
+
+  if (isEmpty(firstName)) {
+    firstName.focus()
+    firstName.style.background = "blue"
+    hashValidate++
+  }
+  if (isEmpty(LastName)) {
+    LastName.focus()
+    LastName.style.background = "blue"
+    hashValidate++
+  }
+  if (!validateEmail(email.value)) {
+    email.focus()
+    email.style.background = "blue"
+    hashValidate++
+  }
+  if (isEmpty(password)) {
+    password.focus()
+    password.style.background = "blue"
+    hashValidate++
+  }
+  return hashValidate
+
+}
+
+function isEmpty(field) {
+  return field.value === ""
 }
 
 function validateEmail(email) {
 
-    const pattern = /^[^ ]+@[^ ]+\.[a-z]{2,3}$/
-  
-    return email.match(pattern)
-  }
-  
-  function resetSubmit() {
-    document.addEventListener("mousedown", (e) => {
-  
-      inputEmail.blur()
-      message.style.visibility = "hidden"
-     
-    })
-  }
+  const pattern = /^[^ ]+@[^ ]+\.[a-z]{2,3}$/
+
+  return email.match(pattern)
+}
+
+function resetSubmit() {
+  document.addEventListener("mousedown", (e) => {
+
+    firstName.blur()
+    LastName.blur()
+    email.blur()
+    password.blur()
+
+    firstName.style.background = "white"
+    LastName.style.background = "white"
+    email.style.background = "white"
+    password.style.background = "white"
+
+
+  })
+}
