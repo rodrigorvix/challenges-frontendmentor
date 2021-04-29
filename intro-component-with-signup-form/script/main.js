e0f429e7d8ebc05de5b0f885265ba9294203a43a
Rodrigo Vitoriense <rodrigovitoriense@gmail.com>
2021-04-28 23:07:04 -0300

    Update script into-component

--- intro-component-with-signup-form/script/main.js
+++ intro-component-with-signup-form/script/main.js
@@ -5,12 +5,13 @@ const email = document.getElementById("email")
 const password = document.getElementById("password")
 const button = document.querySelector(".main-button")
 const inputs = document.querySelectorAll(".main-form-inputs")
+const messages = document.querySelectorAll(".message-error")
 
 form.addEventListener("submit", (e) => {
 
   button.classList.add("main-button-active")
 
-  if (validateForm(firstName, lastName, email, password) === 0) {
+  if (validateForm() === 0) {
     alert("Successful registration !")
 
   } else {
@@ -19,39 +20,38 @@ form.addEventListener("submit", (e) => {
   }
 })
 
-function messageError (input) {
-  const message = document.createElement('span')
-  message.classList.add("message-icon")
-}
-
-function validateForm(firstName, lastName, email, password) {
+function validateForm() {
 
   let hashValidate = 0
 
   if (isEmpty(firstName)) {
+
+    firstName.insertAdjacentHTML('afterend', '<span class="message-error">First Name cannot be empty</span>')
     firstName.focus()
     inputs[0].classList.add("icon-error")
-    messageError.textContent = "testando de novo"
-    inputs[0].appendChild(messageError)
     hashValidate++
   }
   if (isEmpty(lastName)) {
+
+    lastName.insertAdjacentHTML('afterend', '<span class="message-error">Last Name cannot be empty</span>')
     lastName.focus()
-    messageError.textContent = "testando de novo lastname"
-    inputs[1].appendChild(messageError)
-   inputs[1].classList.add("icon-error")
+    inputs[1].classList.add("icon-error")
     hashValidate++
   }
   if (!validateEmail(email)) {
+
+    email.insertAdjacentHTML('afterend', '<span class="message-error">Looks like this is not an email</span>')
+    email.value = "email@example/com"
+    email.style.color = "red"
     email.focus()
-    email.style.background = "blue"
-   inputs[2].classList.add("icon-error")
+    inputs[2].classList.add("icon-error")
     hashValidate++
   }
   if (isEmpty(password)) {
+
+    password.insertAdjacentHTML('afterend', '<span class="message-error">Password cannot be empty</span>')
     password.focus()
-    password.style.background = "blue"
-   inputs[3].classList.add("icon-error")
+    inputs[3].classList.add("icon-error")
     hashValidate++
   }
   return hashValidate
@@ -65,23 +65,20 @@ function isEmpty(field) {
 function validateEmail(email) {
 
   const pattern = /^[^ ]+@[^ ]+\.[a-z]{2,3}$/
-
   return email.value.match(pattern)
 }
 
 function resetSubmit() {
+
   document.addEventListener("mousedown", (e) => {
 
-    firstName.blur()
-    lastName.blur()
-    email.blur()
-    password.blur()
-
-    firstName.style.background = "white"
-    lastName.style.background = "white"
-    email.style.background = "white"
-    password.style.background = "white"
     button.classList.remove("main-button-active")
+
+    inputs.forEach((e) => {
+      e.classList.remove("icon-error")
+      e.blur()
+
+    })
 
 
 
