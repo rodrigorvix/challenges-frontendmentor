cfdf2a7e8b486eaff8f2711b3ea01885d159140f
Rodrigo Vitoriense <rodrigovitoriense@gmail.com>
2021-06-05 19:51:00 -0300

    Update main.js calculator app

--- calculator-app-main/script/main.js
+++ calculator-app-main/script/main.js
@@ -1,73 +1,71 @@
-themeSelect()
-insertDisplay()
-deleteScreen()
-resentScreen()
-calculator()
+initCalculator();
+themeSelect();
 
+function initCalculator() {
+  const buttons = document.querySelectorAll(".buttons-calc button");
+  const display = document.querySelector(".display-calc");
 
-function insertDisplay() {
-  const buttons = document.querySelectorAll(".buttons-calc button")
-  const display = document.querySelector(".display-calc")
-  
-
-  buttons.forEach(button => {
+  buttons.forEach((button) => {
     button.addEventListener("click", (e) => {
-     
-      if(display.innerHTML.length === 1 && display.innerHTML === "0"){
-        display.innerHTML = " "
+      if (e.target.innerText === "DEL") {
+        display.innerHTML = deleteScreen(display.innerHTML);
+      } else if (e.target.innerText === "RESET") {
+        display.innerHTML = resetScreen(display.innerHTML);
+      } else if (e.target.innerText === "=") {
+        const result = calculator(display.innerHTML);
+        display.innerHTML =
+          result === "invalid expression" ? result : formatNumber(result);
+      } else if (display.innerHTML === "0" && display.innerHTML.length === 1) {
+        display.innerHTML = button.innerText;
+      } else {
+        display.innerHTML += button.innerText;
       }
-     
-      if (e.target.innerText !== "DEL" && e.target.innerText !== "RESET" && e.target.innerText !== "=") {
-        display.innerHTML += button.innerText
-      }
-    })
-  })
+    });
+  });
 }
-function deleteScreen() {
-  const delelteButton = document.querySelector('.buttons-calc .del')
-  const display = document.querySelector(".display-calc")
-  
-  delelteButton.addEventListener("click",() => {
-    if(display.innerHTML.length < 1){
-      display.innerHTML = "0"
-    }
-    display.innerHTML = display.innerHTML.slice(0,-1)
-  })
- 
-}
-function resentScreen () {
-  const resetButton = document.querySelector('.buttons-calc .reset')
-  const display = document.querySelector(".display-calc")
-  
-  resetButton.addEventListener("click",() => {
-    display.innerHTML = "0"
-  })
-}
-function calculator () {
-  const display = document.querySelector(".display-calc")
-  const equalButton = document.querySelector('.buttons-calc .equal')
 
-  equalButton.addEventListener("click",() => {
-    display.innerHTML = display.innerHTML.replace(/[x]+/g, "*")
-    display.innerHTML = eval(display.innerHTML)
-  })
-
-
+function deleteScreen(displayCurrent) {
+  if (displayCurrent.length === 1) {
+    displayCurrent = "0";
+  } else {
+    displayCurrent = displayCurrent.slice(0, -1);
+  }
+  return displayCurrent;
 }
+
+function resetScreen(displayCurrent) {
+  return "0";
+}
+
+function calculator(displayCurrent) {
+  displayCurrent = displayCurrent.replace(/[x]+/g, "*");
+  try {
+    return eval(displayCurrent);
+  } catch (error) {
+    return "invalid expression";
+  }
+}
+function formatNumber(number) {
+  if (!Number.isInteger(number)) {
+    number = number.toFixed(3);
+    number = number.toString().replace(/(^0+(?=\d))|(,?0+$)/g, "");
+  }
+  return number;
+}
+
 function themeSelect() {
-  const selectOne = document.querySelector('#theme-one')
-  const selectTwo = document.querySelector('#theme-two')
-  const selectThree = document.querySelector('#theme-three')
-  const html = document.querySelector('html')
+  const selectOne = document.querySelector("#theme-one");
+  const selectTwo = document.querySelector("#theme-two");
+  const selectThree = document.querySelector("#theme-three");
+  const html = document.querySelector("html");
 
-  selectOne.addEventListener("click",() => {
-    html.dataset.theme = "one"
-  })
-  selectTwo.addEventListener("click",() => {
-    html.dataset.theme = "two"
-  })
-  selectThree.addEventListener("click",() => {
-    html.dataset.theme = "three"
-  })
+  selectOne.addEventListener("click", () => {
+    html.dataset.theme = "one";
+  });
+  selectTwo.addEventListener("click", () => {
+    html.dataset.theme = "two";
+  });
+  selectThree.addEventListener("click", () => {
+    html.dataset.theme = "three";
+  });
 }
-
